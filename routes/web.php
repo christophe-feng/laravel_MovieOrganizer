e0cddc656c5ca88612440b0911f902d22f9345ad
ChristopheLin <fengxup@gmail.com>
2026-08-30 16:27:14 +0800

    設定路由

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
+// 首頁直接導向電影清單
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('movies.index');
 });
+
+// 註冊所有 Movie CRUD 路由
+Route::resource('movies', MovieController::class);
